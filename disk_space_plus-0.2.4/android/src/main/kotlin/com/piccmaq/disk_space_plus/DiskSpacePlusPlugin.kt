378ed2dbc156c13191140cbe6de793e5f9643741
dna119@naver.com <dna119@naver.com>
2025-09-06 20:38:11 +0900

    v1 > v2 임베딩 방식 변경

--- disk_space_plus-0.2.4/android/src/main/kotlin/com/piccmaq/disk_space_plus/DiskSpacePlusPlugin.kt
+++ disk_space_plus-0.2.4/android/src/main/kotlin/com/piccmaq/disk_space_plus/DiskSpacePlusPlugin.kt
@@ -6,27 +6,16 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.PluginRegistry
 
 class DiskSpacePlusPlugin: FlutterPlugin {
-
-  companion object {
-    private var channel: MethodChannel? = null
-    private var handler: MethodHandlerImpl = MethodHandlerImpl()
-
-    @JvmStatic
-    fun registerWith(registrar: PluginRegistry.Registrar) {
-      registerChannel(registrar.messenger())
-    }
-
-    private fun registerChannel(messenger: BinaryMessenger) {
-      channel = MethodChannel(messenger, "disk_space_plus")
-      channel!!.setMethodCallHandler(handler)
-    }
-  }
+  private var channel: MethodChannel? = null
+  private val handler = MethodHandlerImpl()
 
   override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    registerChannel(binding.binaryMessenger)
+    channel = MethodChannel(binding.binaryMessenger, "disk_space_plus")
+    channel!!.setMethodCallHandler(handler)
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    channel?.setMethodCallHandler(null)
     channel = null
   }
 }
